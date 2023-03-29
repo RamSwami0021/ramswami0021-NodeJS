845ddbcf7e58a7aa5c32fdc92616fc729dff10b6
Ram Babu <97086308+Rambabu-Metapercept@users.noreply.github.com>
2023-03-29 09:55:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://0.0.0.0:27017/ramswami0021',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post('/sign_up',function(req,res){
+    // taking a user
+    const newuser=new User(req.body);
+    
+   if(newuser.password!=newuser.password2)return res.status(400).json({message: "password not match"});
+    
+    User.findOne({email:newuser.email},function(err,user){
+        if(user) return res.status(400).json({ auth : false, message :"email exits"});
+ 
+        newuser.save((err,doc)=>{
+            if(err) {console.log(err);
+                return res.status(400).json({ success : false});}
+            res.status(200).json({
+                succes:true,
+                user : doc
+            });
+        });
+    });
+ });
+
+app.post('/sign_in', function(req,res){
+    let token=req.cookies.auth;
+    User.findByToken(token,(err,user)=>{
+        if(err) return  res(err);
+        if(user) return res.status(400).json({
+            error :true,
+            message:"You are already logged in"
+        });
+    
+        else{
+            User.findOne({'username':req.body.username},function(err,user){
+                if(!user) return res.json({isAuth : false, message : ' Auth failed ,email not found'});
+        
+                user.comparepassword(req.body.password,(err,isMatch)=>{
+                    if(!isMatch) return res.json({ isAuth : false,message : "password doesn't match"});
+
+            });
+          });
+        }
+    });
+});
+
+app.post("/contact_form",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.message;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "subject": subject,
+        "message" : message
+    }
+
+    db.collection('ContactUs').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Thank You For Contact Us");
+    });
+
+    return res.redirect('/')
+
+})
+
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(8080);
+
+
+console.log("Listening on PORT 8080");
